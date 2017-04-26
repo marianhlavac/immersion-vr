fd74f7d4ee33e968afa41ff6f540ffe055117487
mmajko <hlavac95@gmail.com>
2017-04-26 21:47:19 +0200

    Prepare assets structure

---
+++ Assets/Scripts/AppManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppManager : MonoBehaviour {
+
+    AppManager instance;
+
+    void Awake() {
+        if (instance != this) {
+            instance = this;
+        } else {
+            Destroy(instance);
+            instance = this;
+        }
+
+        string ownedGames = SteamAPI.getOwnedGames(delegate (string result) {
+            print(result);
+        });
+
+        print(ownedGames);
+    }
+	
+}
