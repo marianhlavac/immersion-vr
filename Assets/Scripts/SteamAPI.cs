9b6443711dcf85b099bcb96615bc43086c4535a7
mmajko <hlavac95@gmail.com>
2017-05-03 19:52:05 +0200

    Add other assets

---
+++ Assets/Scripts/SteamAPI.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public static class SteamAPI {
+
+	public static string apiBaseUrl = "http://api.steampowered.com";
+	public static string apiKey = "F3DEFFF53EEA9F6480BF82B654A5C3F3"; // TODO: this must gtfo
+
+	private static string makeUrl(string iface, string method, string version, string parameters) {
+		return apiBaseUrl + "/" + iface + "/" + method + "/" + 
+			version + "?key=" + apiKey + (parameters != "" ? "&" + parameters : "");
+	}
+
+	private static string makeUrl(string iface, string method, string version) {
+		return makeUrl (iface, method, version, "");
+	}
+
+
+    public static IEnumerator makeRequest(string URL) {
+        WWW www = new WWW(URL);
+        Debug.Log("Yielding");
+        yield return www;
+
+        Debug.Log("Yielding done");
+        Debug.Log(www.text);
+    }
+
+    public static void getOwnedGames() {
+        string url = makeUrl("IPlayerService", "GetOwnedGames", "v0001", "steamid=76561198006300545&include_appinfo=1&include_played_free_games=1");
+
+        makeRequest(url);
+	}
+}
---
+++ Assets/Scripts/TutorialManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public enum TutorialPhase {
+    NotRunning,
+    Intro, Intro2,
+    Skippable,
+    PlayArea,
+    Controllers,
+    ButtonsTrigger, ButtonsSide, ButtonsTouch, ButtonsRest,
+    Launcher
+};
+
+public class CuePlayException : Exception {
+    public CuePlayException(string message) : base(message) { }
+}
+
+public class TutorialManager : MonoBehaviour {
+    public TutorialPhase startingPhase;
+    public AudioClip[] audioCues;
+
+    private TutorialPhase phase;
+
+    // Use this for initialization
+    void Start () {
+        phase = startingPhase;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void AdvanceTutorial () {
+
+    }
+
+    void PlayAudioCue (uint id) {
+        AudioSource source = GetComponent<AudioSource>();
+
+        if (id < 0 || id >= audioCues.Length) {
+            throw new CuePlayException("Audio cue with specified ID not found.");
+        }
+
+        source.clip = audioCues[id];
+        source.Play();
+    }
+}
